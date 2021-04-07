7119cfe7283f95fd2db8c3d1ba8855f55d814a9f
Rafia Zafar <rafiazafar75@gmail.com>
2021-04-07 16:53:02 +0500

    setting some changes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import FrontPage from './layout/FrontPage';
 const  App =()=> {
  return <div>
      <Router>
-       <MainNavigation/>
+       
    <Switch>
    <Route path="/" exact>
        <FrontPage />
    </Route>
+   
    <Route path="/Items" exact>
+   <MainNavigation/>
       <Item/>
    </Route>
    <Redirect to="/"/>
