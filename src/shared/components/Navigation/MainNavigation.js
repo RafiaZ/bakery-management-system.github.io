48cb49bde826b2cd53438d6f3934654f812e7cd4
Rafia Zafar <rafiazafar75@gmail.com>
2021-04-10 14:19:43 +0500

    adding drwaer properties

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 import { Link } from "react-router-dom";
 import SideDrawer from "./SideDrawer";
 import NavLinks from "../Navigation/NavLinks";
@@ -8,13 +8,18 @@ import Header from "./Header";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [DrawerIsOpen, setDrawerIsOpen] = useState(flase);
+
+  setDrawerIsOpen=()=>{
+    useState(true);
+  }
   return (
     <React.Fragment>
       <SideDrawer>
           <nav className="main-navigation__drawer-nav"><NavLinks/></nav>
       </SideDrawer>
       <Header>
-        <button className="main-header__button">
+        <button className="main-header__button" onClick={setDrawerIsOpen}>
           <span />
           <span />
           <span />
